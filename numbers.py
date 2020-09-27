bc4502c2823603c1fa57b0a222fbbd1203d74061
dumandeniz <deniz.duman1@ogr.sakarya.edu.tr>
2020-09-27 17:50:59 +0300

    first commit

---
+++ numbers.py
@@ -0,0 +1,11 @@
+maasAli = 3750
+maasAhmet = 4232
+vergi = 0.27
+
+print(maasAli - (maasAli*vergi))
+print(maasAhmet - (maasAhmet*vergi))
+
+sayi = 2
+print(type(sayi))
+sayi = "string oldu"
+print(sayi)
