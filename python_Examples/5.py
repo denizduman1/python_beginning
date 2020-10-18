eaacb4e84155485284c593dd81ab071cda5b7333
denizduman1 <ce.denizduman@gmail.com>
2020-10-18 16:27:19 +0300

    add

--- python_Examples/5.py
+++ python_Examples/5.py
@@ -1,33 +1,59 @@
-import random
+# import random
 
-sayilar = [1,2,3,4,5]
-sayisal_Loto = []
-tahmin = []
-i=0
+# sayilar = [1,2,3,4,5]
+# sayisal_Loto = []
+# tahmin = []
+# i=0
 
-while i<5:
-    sayi = int(input("Tahmin Gir= "))
-    tahmin.append(sayi)
-    j = random.randint(0,4)
-    sayisal_Loto.append(sayilar[j])
-    i += 1
+# while i<5:
+#     sayi = int(input("Tahmin Gir= "))
+#     tahmin.append(sayi)
+#     j = random.randint(0,4)
+#     sayisal_Loto.append(sayilar[j])
+#     i += 1
     
-score = 0
-for i in range(0,5):
-    if(sayisal_Loto[i]==tahmin[i]):
-        score += 1
-    else:
-        continue
+# score = 0
+# for i in range(0,5):
+#     if(sayisal_Loto[i]==tahmin[i]):
+#         score += 1
+#     else:
+#         continue
     
-print("Sayısal Loto Sonuçlar = ",end="")
+# print("Sayısal Loto Sonuçlar = ",end="")
 
-j=0
+# j=0
 
-for i in sayisal_Loto:
-    if j<4:
-        print(i,",",end=" ")
-    else:
-        print(i)
-    j += 1
+# for i in sayisal_Loto:
+#     if j<4:
+#         print(i,",",end=" ")
+#     else:
+#         print(i)
+#     j += 1
 
-print("Scorunuz = "  , score , "/ 5")
+# print("Scorunuz = "  , score , "/ 5")
+
+# sayi = int(input("Sayı Gir = "))
+# i = 1
+# toplam = 0
+# while i<sayi:
+#     if(sayi%i==0):
+#         toplam = toplam + i
+#     i = i + 1
+
+# if(toplam == sayi):
+#     print("Sayınız Mükemmel Sayıdır.")
+# else: 
+#     print("Sayınız Mükemmel Sayı Değildir.")
+
+bas_sayi = int(input("başlangıç sayı Gir = "))
+son_sayi = int(input("son sayı Gir = "))
+
+for i in range(bas_sayi,son_sayi):
+    j = 1
+    toplam = 0
+    while j < i :
+        if(i%j==0):  
+            toplam = toplam + j
+        j = j + 1
+    if(toplam == i):
+        print(i,end=" ")
